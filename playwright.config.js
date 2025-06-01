b0a112ef8a7d0b498bef1ba1bc31b710f2fc826d
syeda1990 <syeda.siddiqua.bebe@gmail.com>
2025-06-01 19:14:41 +0200

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -9,6 +9,7 @@ import { defineConfig, devices } from '@playwright/test';
 // import path from 'path';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
+
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
